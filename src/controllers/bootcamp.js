fa581a9b0337036f60afd79509741c665fe55373
Paras-code-007 <paras.arora0025@gmail.com>
2020-12-19 11:27:58 +0530

    create sample controllers for bootcamp route

---
+++ src/controllers/bootcamp.js
@@ -0,0 +1,24 @@
+// @desc    Get all bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  Public
+exports.getAllBootcamps = (req, res, next) => {};
+
+// @desc    Get all bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  Public
+exports.getBootcamp = (req, res, next) => {};
+
+// @desc    Get all bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  Public
+exports.createBootcamp = (req, res, next) => {};
+
+// @desc    Get all bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  Public
+exports.updateBootcamp = (req, res, next) => {};
+
+// @desc    Get all bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  Public
+exports.deleteBootcamp = (req, res, next) => {};
